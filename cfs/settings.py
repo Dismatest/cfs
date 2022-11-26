7670eebe3c3dfce9419dd892bb57844086894005
Dismatest <billclintonogot88@gmail.com>
2022-11-26 23:24:22 +0300

    Adding procfile, requirment.tx,and runtime.txt

--- cfs/settings.py
+++ cfs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mzgef+7p#%z5ldcxyzsm+w%wy)4hwjc5%(!8hb^ko$006s!l@&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
